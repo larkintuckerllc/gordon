35e13040976cfd02e7b530f197ef3a5d0513de59
John Tucker <jtucker@spotify.com>
2021-07-27 21:35:22 -0400

    done

--- internal/gordon/parse.go
+++ internal/gordon/parse.go
@@ -1,20 +1,64 @@
 package gordon
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type PubSubMessage struct {
 	Message struct {
-		Data []byte `json:"data,omitempty"`
-		ID   string `json:"id"`
+		Data []byte `json:"data"`
 	} `json:"message"`
-	Subscription string `json:"subscription"`
 }
 
-func parse(data []byte) (*PubSubMessage, error) {
-	var m PubSubMessage
-	err := json.Unmarshal(data, &m)
+type Data struct {
+	ProtoPayload struct {
+		MethodName string `json:"methodName"`
+	} `json:"protoPayload"`
+	Resource struct {
+		Labels struct {
+			InstanceId string `json:"instance_id"`
+			Zone       string `json:"zone"`
+		} `json:"labels"`
+	} `json:"resource"`
+}
+
+type Method int
+
+const (
+	Allocate Method = iota
+	Deallocate
+)
+
+type Instance struct {
+	InstanceId string
+	Zone       string
+}
+
+func parse(data *[]byte) (*Method, *Instance, error) {
+	var p PubSubMessage
+	var d Data
+	var m Method
+	err := json.Unmarshal(*data, &p)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return &m, nil
+	err = json.Unmarshal(p.Message.Data, &d)
+	if err != nil {
+		return nil, nil, err
+	}
+	switch d.ProtoPayload.MethodName {
+	case "beta.compute.instances.insert":
+		m = Allocate
+	case "v1.compute.instances.stop":
+		m = Deallocate
+	case "v1.compute.instances.start":
+		m = Allocate
+	case "v1.compute.instances.delete":
+		m = Deallocate
+	default:
+		return nil, nil, errors.New("invalid MethodName")
+	}
+	i := Instance{d.Resource.Labels.InstanceId, d.Resource.Labels.Zone}
+	return &m, &i, nil
 }
